3a5b211f2870f8ccb41bd887c3f8d20c6b052730
tanjim750 <mdtanjim750@gmail.com>
2024-02-13 00:16:15 +0600

    WIP

---
+++ manage.py
@@ -0,0 +1,37 @@
+from colorama import Fore, Style
+import os
+import re
+
+def extract_domain(url):
+    match = re.search(r'https?://(?:www\.)?([a-zA-Z0-9.-]+)', url)
+    return match.group(1) if match else None
+
+def validate_url(url):
+    # Regular expression for a basic URL validation
+    url_pattern = re.compile(
+        r'^(https?|ftp):\/\/'  # http:// or https:// or ftp://
+        r'(www\.)?'             # optional www
+        r'[a-zA-Z0-9.-]+'       # domain name
+        r'(\.[a-zA-Z]{2,})'     # dot-something
+        r'(:\d{1,5})?'          # optional port
+        r'([\/?#].*)?$'         # path, query parameters, or fragment
+    )
+
+    return bool(re.match(url_pattern, url))
+
+def input_text(text=None,color=None):
+    if text is None:
+        text_ = Fore.RED+"\n╔═══"+Fore.CYAN+"["+Fore.GREEN+"Tanjim"+Fore.CYAN+"]"+Fore.RED+"══════"+Fore.CYAN+"["+Fore.GREEN+"HuntApi"+Fore.CYAN+"]"+"\n"+Fore.RED+"║"+"\n"+Fore.RED+"╚═══➣➣ "+Fore.GREEN
+    else:
+        text_ = Fore.RED+"\n╔═══"+Fore.CYAN+"["+color+text+Fore.CYAN+"]"+"\n"+Fore.RED+"║"+"\n"+Fore.RED+"╚═══➣➣ "+Fore.GREEN
+    return text_
+
+
+web_url = input(input_text("Enter your target Web URL(with 'http or https')",Fore.GREEN))
+if not validate_url(web_url): # if domain can be extracted then the url is valid
+    web_url = ""
+
+while len(web_url) < 1:
+    web_url = input(input_text('Enter a valid Web URL(with "http or https")',Fore.RED))
+    if not validate_url(web_url): # if domain can be extracted then the url is valid
+        web_url = ""
---
+++ replicate.sh
@@ -0,0 +1,23 @@
+#usr/bin/bash
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m' 
+red='\033[31;1m' 
+white='\033[37;1m' 
+yellow='\033[33;1m' 
+
+
+echo
+echo
+echo
+echo $cyan"       ░  █   █"$cyan" █   █"$cyan" █▀▄   █"$cyan" ▀▀█▀▀"$green"   ▄▀▀▀▄  █▀▀▀▄ ▀▀█▀▀  ░"
+echo $cyan"       ░  █▀▀▀█"$cyan" █   █"$cyan" █  ▀▄ █"$cyan"   █  "$green"   █▄▄▄█  █▄▄▄▀   █    ░"
+echo $cyan"       ░  █   █"$cyan" █▄▄▄█"$cyan" █    ▀█"$cyan"   █  "$green"   █   █  █     ▄▄█▄▄  ░"
+echo
+
+
+echo $green "Welcome to website cloner"$purple "Web Replica"
+echo $cyan "Autor Tanjim Abubokor"
+
+python manage.py
